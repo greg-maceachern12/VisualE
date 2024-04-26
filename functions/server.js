d47dff4ad892f207ccf410f8c7dea7da54248ccc
greg-maceachern12 <gregmaceachern98@gmail.com>
2024-04-26 10:45:35 -0400

    netlify-test

--- functions/server.js
+++ functions/server.js
@@ -83,8 +83,8 @@ app.post("/generateImage", async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 module.exports.handler = serverless(app);
