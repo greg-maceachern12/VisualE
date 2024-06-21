27a0dde398e22e829568de771fde10142bace53f
greg-maceachern12 <gregmaceachern98@gmail.com>
2024-06-21 08:12:55 -0400

    string

--- src/About.js
+++ src/About.js
@@ -19,13 +19,13 @@ function About() {
         </Link>
       <p className="aboutText">
         Welcome to Visuai - a project inspired by the rich visual
-        descriptions found in recent fictional books I've been immersed in, from
+        descriptions found in recent fictional books I've been reading, from
         the likes of the Dune series, the Way of Kings and Eragon.
         <br></br><br></br>
         <i>
-          While reading these books, I discovered that authors significantly invest in crafting rich
-          visual details for many of their scenes, environments, characters etc. My brain (and many others) struggles to fully
-          comprehend and visualize them. I often found myself searching Google for fan renditions of
+          While reading these books, I found that that authors significantly invest in crafting rich
+          visual details for many of their environments, characters, scenes etc. My brain struggled to fully
+          comprehend and visualize this. I often found myself searching Google for fan renditions of
           the characters to aid my imagination rather than lean on the descriptions provided by the author.
         </i>
         <br></br>
@@ -38,37 +38,31 @@ function About() {
         an interface to connect these elements together.
         <br></br>
         <br></br>
-        Hence, this app.
         <br></br>
-        <br></br>
-        IMO, the best user experience would be a Kindle/iBooks extension directly
+        I would love to ship this as a Kindle/iBooks extension, directly
         integrated into the reading experience, automatically generating visuals for the
         user or based on highlighted content. Until Amazon or Apple opens up an
         extensions marketplace, Visuai will operate as a self-serve platform.
         <br></br><br></br>
         For now, users can browse chapter by chapter and/or select a specific
-        chapter <i>(coming soon)</i> to explore a visual representation of that
+        chapter to explore a visual representation of that
         portion of the book. In the <a href='pro.visuai.app' target="_blank" rel="noreferrer">pro version</a>, you can upload a whole epub and Visuai will generate visuals for the entire book,
         adding them to the beginning of each chapter.
         <br></br><br></br>
         There are numerous improvements planned for Visuai, including:
         <ol>
-          <li>Move to Groq for faster inference on parts of the generation.</li>
           <li>
             Migrate to Midjourney for consistent characters in the generation
             (massively important!) once the API is opened up.
           </li>
           <li>Utilization of control nets for enhanced image generation</li>
-          <li>
-            Caching of AI-generated images for future use on the same book
-          </li>
           <li>Implementation of a subscription model and/or payment schema</li>
         </ol>
         Until then, please enjoy!<br></br>
         If you're reading this{" "}
         <b>
-          please don't go crazy on the generations.. this eats up my OpenAI
-          credit :(
+          please don't go crazy on the generations.. this eats up my OpenAI/SD
+          credits
         </b>
         <br></br>
         <br></br>
